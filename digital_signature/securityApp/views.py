cb935701f0341eaacde2e9b39589cc3d78f5d6f8
arian <arian_gh22@yahoo.com>
2024-07-10 19:04:14 +0330

    adding the functionality of signing and verifying and in addition some other staffs to my code

--- digital_signature/securityApp/views.py
+++ digital_signature/securityApp/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
 import rsa
 import base64
 
 class Signature :
-    def __init__(self, signDate="", signTime="", privateKeyFile="/django2/myapp/keys/server/private.pem", publicKeyFile="/django2/myapp/keys/server/public.pem") :
+    def __init__(self, signDate="", signTime="", privateKeyFile=r"D:\arian\repos\Digital-Signature\digital_signature\server\private.pem", publicKeyFile=r"D:\arian\repos\Digital-Signature\digital_signature\server\public.pem") :
         self.signDate = signDate
         self.signTime = signTime
         self.privateKeyFile = privateKeyFile
@@ -24,3 +26,29 @@ class Signature :
             return True
         except:
             return False
+
+
+
+
+@api_view(["GET"])
+def sign(request):
+    # Generate RSA keys with a key size of 2048 bits
+    (public_key, private_key) = rsa.newkeys(2048)
+    with open(r"D:\arian\repos\Digital-Signature\digital_signature\server\public.pem", "rb") as public_key_file:
+        pem_public_key = public_key_file.read()
+    # Example string to sign
+    string_to_sign = "sdasdasdasd"
+
+    # Sign the string using the private key
+    signature = rsa.sign(string_to_sign.encode(), private_key, 'SHA-256')
+
+    # Serialize the public key to PEM format
+    pem_public_key = public_key.save_pkcs1().decode('utf-8')
+
+    # Prepare the response
+    response_data = {
+        'public_key': pem_public_key,
+        'signature': signature.hex()  # Convert signature to hex string for easy display
+    }
+
+    return Response(response_data)
